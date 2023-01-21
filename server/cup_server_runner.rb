b04658fd07b5bc16046e1fa3967293bd0b98d295
aaaasmile <testhaus@gmx.at>
2023-01-21 23:04:56 +0100

    stop server with trap

--- server/cup_server_runner.rb
+++ server/cup_server_runner.rb
@@ -15,13 +15,6 @@ require "nal_srv_algorithm"
 require "cuperativa_server"
 require "database/sendemail_errors"
 
-$g_os_type = :win32_system
-begin
-  require "win32/sound"
-rescue LoadError
-  $g_os_type = :linux
-end
-
 include Log4r
 
 module MyGameServer
@@ -32,13 +25,21 @@ module MyGameServer
     def initialize
       @serv_settings = {}
       @settings_default = {
-        :ip => "127.0.0.1", :port => 20606, :login_email => "",
-        :password_email => "", :publickey_server => "00000", :secret_server => "00",
+        :ip => "127.0.0.1", :port => 20606,
+        :email_crash => {
+          :email_to => "",
+          :email_relayfrom => "",
+          :relay_user => "",
+          :relay_secret => "",
+          :relay_host => "",
+          :send_email => false,
+        },
+        :publickey_server => "00000", :secret_server => "00",
         :test_local_service => true, :logpath => "../logs",
         :database => {
           :user_db => "",
           :pasw_db => "",
-          :use_sqlite3 => false,
+          :module => "pg",
           :db_name => "cupuserdatadb",
         },
         :connhandler_opt => {},
@@ -51,7 +52,7 @@ module MyGameServer
     def initlog(target)
       begin
         #p @serv_settings
-        @logger = Log4r::Logger.new("serv_main")
+        @log = Log4r::Logger.new("serv_main")
         logpath_abs = File.expand_path(@serv_settings[:logpath], __FILE__)
         out_log_name = File.join(logpath_abs, "#{Time.now.strftime("%Y_%m_%d_%H_%M_%S")}cup_server.log")
         mystderr_file = File.join(logpath_abs, "cup_stderr#{Time.now.strftime("%Y_%m_%d_%H_%M_%S")}.log")
@@ -62,29 +63,28 @@ module MyGameServer
           :maxtime => 86400, # tempo in secondi (1 * 14 giorni). Dopo 14 giorni avviene il rollout e
           # quindi viene creato un nuovo file
           :filename => out_log_name,
-          :trunc => true, # se true viene usato 'w' in File.open, altrimenti con false 'a'
-          # voglio 'a' in quanto ogni volta che viene chiamato lo script, devo avere un append
+          :trunc => true, # with true a new file is created, otherwise append
           :formatter => myApacheLikeFormat,
         })
 
         Log4r::Logger["serv_main"].add "serv_main"
         # open a file for standard error, usefull to cache emachine errors
         STDERR.reopen(mystderr_file, "w")
-        #@logger.outputters =  mybaseApacheLikeLog
+        #@log.outputters =  mybaseApacheLikeLog
         if target == :test
-          @logger.outputters << Outputter.stdout
+          @log.outputters << Outputter.stdout
         else
-          @logger.level = INFO
+          @log.level = INFO
         end
         # now we can get the server beacuse logger is initiated
         @main_my_srv = CuperativaServer.instance
         @main_my_srv.set_dir_log(logpath_abs)
-        @logger.debug "Init #{logpath_abs} log ok"
+        @log.debug "Init log dir #{logpath_abs} ok"
       rescue
         str_err = "Server crashed error: #{$!}"
         str_err += detail.backtrace.join("\n")
         p "error #{str_err}"
-        @logger.error "error #{str_err}"
+        @log.error "error #{str_err}"
         exit
       end
     end
@@ -108,38 +108,42 @@ module MyGameServer
     end
 
     def run
+      @log.info "Run the server"
       @main_my_srv.set_settings(@serv_settings)
       stopped_by_shutdown = false
 
-      # avoid brutal shutdown with ctr-c
       trap(:INT) {
-        @logger.info("Shutdown the server")
+        #shutdown with ctr-c
+        p "Shutdown the server" # do not use @log in trap
         EventMachine::stop
         stopped_by_shutdown = true
-        # set server info for OFFLINE modus
       }
-      if $g_os_type == :win32_system
-        stopped_by_shutdown = true #on windows trap doesn't work
-      end
 
       while true
         # when the server crash because an error, then restart it
         # dont' restart it only if it was :INT trap
         begin
           go_server_go() # blocking call, exit only when server is stopped for some reason
-          @logger.info "Server is now OFF"
+          @log.info "Server is now OFF"
         rescue => detail
-          @logger.error "Server crashed error(#{$!})"
-          @logger.error(detail)
+          @log.error "Server crashed error(#{$!})"
+          @log.error(detail)
           str_err = "Server crashed error: #{$!}"
           str_err += detail.backtrace.join("\n")
-          sender = EmailErrorSender.new(@logger)
-          sender.send_email("Match server error:\n" + ("#{str_err}\n"))
+          if @serv_settings[:email_crash][:send_email]
+            sender = EmailErrorSender.new(@log)
+            sender.send_email("Match server error:\n" + ("#{str_err}\n"))
+          end
+          #p "error #{detail}"
         ensure
-          break if stopped_by_shutdown
-          @logger.info "Restarting the server..."
+          if stopped_by_shutdown
+            break
+          else
+            @log.info "Restarting the server..."
+          end
         end
       end
+      @log.info "Server turning off because run terminated"
     end
 
     def go_server_go
@@ -147,18 +151,14 @@ module MyGameServer
         host = @serv_settings[:ip]
         port = @serv_settings[:port]
 
-        # connect to the db
-        @main_my_srv.connect_to_db(@serv_settings[:database][:user_db],
-                                   @serv_settings[:database][:pasw_db],
-                                   @serv_settings[:database][:db_name],
-                                   @serv_settings[:database][:use_sqlite3])
+        #@main_my_srv.connect_to_db(@serv_settings[:database])
 
         #
         # start the game server
         EventMachine::start_server(host, port, MyGameServer::CuperativaUserConn)
         #p a.class # class is string
         #p a # something like "efa022d6-dd65-41b7-b84f-d4afeec0b5392"
-        @logger.info("*** Now accepting connections on address #{host}:#{port}")
+        @log.info("*** Now accepting connections on address #{host}:#{port}")
         #EventMachine::add_periodic_timer( 10 ) { $stderr.write "*" }
         EventMachine::add_periodic_timer(300) { @main_my_srv.ping_clients() }
         EventMachine::add_periodic_timer(0.03) { @main_my_srv.process_game_in_progress }
--- server/database/sendemail_errors.rb
+++ server/database/sendemail_errors.rb
@@ -1,11 +1,42 @@
 #file: sendemail_errors.rb
 # Use to send an email when an error occours.
 
-require 'rubygems'
-require 'net/smtp'
+# TODO:
+# E-Mail should be sent using the relay with tls and authentication.
+# The pseudo code is:
+#   ms.emailTo = ms.secret.Email
+# 	ms.emailFrom = ms.secret.RelayMail
+# 	servername := ms.secret.RelayHost
+
+# 	host, _, _ := net.SplitHostPort(servername)
+
+# 	auth := smtp.PlainAuth("", ms.secret.RelayUser, ms.secret.RelaySecret, host)
+
+# 	tlsconfig := &tls.Config{
+# 		InsecureSkipVerify: true,
+# 		ServerName:         host,
+# 	}
+
+# 	log.Println("Dial server ", servername)
+# 	conn, err := tls.Dial("tcp", servername, tlsconfig)
+# 	if err != nil {
+# 		return err
+# 	}
+
+# 	c, err := smtp.NewClient(conn, host)
+# 	if err != nil {
+# 		return err
+# 	}
+
+# 	log.Println("Send smtp Auth")
+# 	if err = c.Auth(auth); err != nil {
+# 		return err
+# 	}
+
+require "rubygems"
+require "net/smtp"
 
 class EmailErrorSender
-  
   def initialize(log, dest_email, from_email)
     @log = log
     @destination = dest_email
@@ -15,11 +46,11 @@ class EmailErrorSender
 
   def send_email(detail)
     msgstr = <<END_OF_MESSAGE
-From: Cuperativa <igor@invido.it>
+From: Cuperativa <#{@from}>
 Subject: Errore (#{Time.now}) da Cup.invido.it
 Date: #{Time.now}
 Mime-Version: 1.0
-Content-Type: text/plain; charset=ISO-8859-1
+Content-Type: text/plain; charset=UTF-8
 Message-Id: <#{random_tag()}@invido.it>
 
 Ciao Igor,
@@ -33,41 +64,38 @@ fai come vuoi,
 Cuperativa Server
 
 END_OF_MESSAGE
-    
+
     #p msgstr
-       Net::SMTP.start('localhost', 25) do |smtp|
-         smtp.send_message msgstr,
-                           @from,
-                           @destination
-       end
-       
-       @log.debug("Email with log report was sent OK.")
-    rescue
-      # send error
-      @log.error("send_email error detail is failed. Reason #{$!}")
+    Net::SMTP.start("localhost", 25) do |smtp|
+      smtp.send_message msgstr,
+                        @from,
+                        @destination
+    end
+
+    @log.debug("Email with log report was sent OK.")
+  rescue
+    # send error
+    @log.error("send_email error detail is failed. Reason #{$!}")
   end
-  
+
   ##
   # Provides a random tag
   def random_tag
     @uniq += 1
     t = Time.now
-    sprintf('%x%x_%x%x%d%x',
+    sprintf("%x%x_%x%x%d%x",
             t.to_i, t.tv_usec,
             $$, Thread.current.object_id, @uniq, rand(255))
   end
-  
 end
-       
-       
+
 if $0 == __FILE__
-  require 'log4r'
+  require "log4r"
   include Log4r
-  
+
   mylog = Log4r::Logger.new("email_log_notifier")
-  mylog.add 'stdout'
-  
+  mylog.add "stdout"
+
   sender = EmailErrorSender.new(mylog)
   sender.send_email("Un error grave nel file aa.eb linea 123.")
-  
 end
